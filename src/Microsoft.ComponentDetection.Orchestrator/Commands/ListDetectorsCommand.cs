21a3e05e69b2fb435135cbdedb76ef9018640521
Jamie Magee <jamagee@microsoft.com>
2023-04-21 09:01:34 -0700

    refactor: migrate to `Spectre.Console.Cli`

---
+++ src/Microsoft.ComponentDetection.Orchestrator/Commands/ListDetectorsCommand.cs
@@ -0,0 +1,37 @@
+﻿namespace Microsoft.ComponentDetection.Orchestrator.Commands;
+
+using System.Collections.Generic;
+using Microsoft.ComponentDetection.Contracts;
+using Microsoft.ComponentDetection.Orchestrator.Services;
+using Microsoft.Extensions.Logging;
+using Spectre.Console.Cli;
+
+/// <summary>
+/// Lists available detectors.
+/// </summary>
+public sealed class ListDetectorsCommand : Command
+{
+    private readonly IEnumerable<IComponentDetector> detectors;
+    private readonly ILogger<DetectorListingCommandService> logger;
+
+    public ListDetectorsCommand(
+        IEnumerable<IComponentDetector> detectors,
+        ILogger<DetectorListingCommandService> logger)
+    {
+        this.detectors = detectors;
+        this.logger = logger;
+    }
+
+    /// <inheritdoc/>
+    public override int Execute(CommandContext context)
+    {
+        this.logger.LogInformation("Detectors:");
+
+        foreach (var detector in this.detectors)
+        {
+            this.logger.LogInformation("{DetectorId}", detector.Id);
+        }
+
+        return (int)ProcessingResultCode.Success;
+    }
+}
--- src/Microsoft.ComponentDetection/Program.cs
+++ src/Microsoft.ComponentDetection/Program.cs
@@ -2,52 +2,60 @@
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.ComponentDetection.Contracts;
-using Microsoft.ComponentDetection.Orchestrator;
+using Microsoft.ComponentDetection.Orchestrator.Commands;
 using Microsoft.ComponentDetection.Orchestrator.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
+using Spectre.Console.Cli;
+using Spectre.Console.Cli.Extensions.DependencyInjection;
 
-try
+if (args.Contains("--debug", StringComparer.OrdinalIgnoreCase))
 {
-    if (args.Contains("--debug", StringComparer.OrdinalIgnoreCase))
+    Console.WriteLine($"Waiting for debugger attach. PID: {Process.GetCurrentProcess().Id}");
+    while (!Debugger.IsAttached)
     {
-        Console.WriteLine($"Waiting for debugger attach. PID: {Process.GetCurrentProcess().Id}");
-        while (!Debugger.IsAttached)
-        {
-            await Task.Delay(1000);
-        }
+        await Task.Delay(1000);
     }
+}
 
-    Log.Logger = new LoggerConfiguration()
-        .WriteTo.Console(outputTemplate: "[BOOTSTRAP] [{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
-        .CreateBootstrapLogger();
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console(outputTemplate: "[BOOTSTRAP] [{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
+    .CreateBootstrapLogger();
 
-    var serviceProvider = new ServiceCollection()
-        .AddComponentDetection()
-        .ConfigureLoggingProviders()
-        .BuildServiceProvider();
-    var orchestrator = serviceProvider.GetRequiredService<Orchestrator>();
-    var result = await orchestrator.LoadAsync(args);
-
-    var exitCode = (int)result.ResultCode;
-    if (result.ResultCode is ProcessingResultCode.Error or ProcessingResultCode.InputError)
+// var serviceProvider = new ServiceCollection()
+//     .AddComponentDetection()
+//     .ConfigureLoggingProviders()
+//     .BuildServiceProvider();
+// var orchestrator = serviceProvider.GetRequiredService<Orchestrator>();
+// var result = await orchestrator.LoadAsync(args);
+var serviceCollection = new ServiceCollection()
+    .AddComponentDetection()
+    .ConfigureLoggingProviders();
+using var registrar = new DependencyInjectionRegistrar(serviceCollection);
+var app = new CommandApp<ListDetectorsCommand>(registrar);
+app.Configure(
+    config =>
     {
-        exitCode = -1;
-    }
+        config.Settings.ApplicationName = "component-detection";
+        config.CaseSensitivity(CaseSensitivity.None);
 
-    Console.WriteLine($"Execution finished, status: {exitCode}.");
+        config.AddCommand<ListDetectorsCommand>("list-detectors")
+            .WithDescription("Lists available detectors");
+    });
+return await app.RunAsync(args).ConfigureAwait(false);
 
-    // Manually dispose to flush logs as we force exit
-    await serviceProvider.DisposeAsync();
-
-    await Log.CloseAndFlushAsync();
-
-    // force an exit, not letting any lingering threads not responding.
-    Environment.Exit(exitCode);
-}
-catch (ArgumentException ae)
-{
-    await Console.Error.WriteLineAsync(ae.ToString());
-    Environment.Exit(-1);
-}
+// var exitCode = (int)result.ResultCode;
+// if (result.ResultCode is ProcessingResultCode.Error or ProcessingResultCode.InputError)
+// {
+//     exitCode = -1;
+// }
+//
+// Console.WriteLine($"Execution finished, status: {exitCode}.");
+//
+// // Manually dispose to flush logs as we force exit
+// await serviceProvider.DisposeAsync();
+//
+// await Log.CloseAndFlushAsync();
+//
+// // force an exit, not letting any lingering threads not responding.
+// Environment.Exit(exitCode);
